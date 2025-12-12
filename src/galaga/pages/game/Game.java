2d84bf56c0d479bb44feec902ca04f418c21f11f
jules1univ <julesgarcia.bzh@gmail.com>
2025-12-12 10:02:05 +0100

    safe check if no skin is selected

--- src/galaga/pages/game/Game.java
+++ src/galaga/pages/game/Game.java
@@ -40,7 +40,11 @@ public class Game extends Page<GalagaPage> {
     }
 
     @Override
-    public boolean onActivate() {        
+    public boolean onActivate() {   
+        if(Galaga.getContext().getState().shipSkin == null) {
+            Galaga.getContext().getState().shipSkin = Galaga.getContext().getResource().get(Config.SPRITES_SHIP.get(0));
+        }
+        
         this.particles = new ParticlesManager();
         this.bullets = new BulletManager();
 
@@ -106,7 +110,7 @@ public class Game extends Page<GalagaPage> {
         if(this.enemies == null || this.enemies.isEmpty()) {
             return false; 
         }
-        
+
         for (Enemy enemy : this.enemies) {
             if (!enemy.init()) {
                 return false;
