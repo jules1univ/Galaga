138d0dc5ac35806ebf08b743f8c7543259cfc65c
Satch3 <sachaglm02@gmail.com>
2025-12-19 11:04:25 +0100

    continue editor menu

--- src/galaga/pages/editor/menu/EditorMenu.java
+++ src/galaga/pages/editor/menu/EditorMenu.java
@@ -2,25 +2,31 @@ package galaga.pages.editor.menu;
 
 import engine.elements.page.Page;
 import engine.elements.ui.select.TextSelect;
+import engine.elements.ui.text.Text;
+import engine.elements.ui.text.TextPosition;
 import engine.resource.sound.Sound;
 import engine.utils.Position;
 import engine.utils.Size;
 import galaga.Config;
 import galaga.Galaga;
 import galaga.GalagaPage;
+import galaga.GalagaSound;
 import galaga.entities.sky.Sky;
-import galaga.pages.menu.MenuModeOption;
-import galaga.pages.menu.MenuOption;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 
+
+
 public class EditorMenu extends Page<GalagaPage> {
 
-    TextSelect editSelect;
+    private TextSelect editSelect;
+    private Text back;
+    
     private Font titleFont;
     private Sky sky;
+
+    private MenuEditorOption option;
 
     private Sound themeSound;
     private Sound selectSound;
@@ -37,6 +43,21 @@ public class EditorMenu extends Page<GalagaPage> {
                 Galaga.getContext().getFrame().getHeight());
 
         this.titleFont = Galaga.getContext().getResource().get(Config.FONTS, Config.VARIANT_FONT_XLARGE);
+
+        this.themeSound = Galaga.getContext().getResource().get(GalagaSound.menu_theme);
+        if (this.themeSound == null) {
+            return false;
+        }
+        this.themeSound.setLoop(true);
+        this.themeSound.play(0.2f);
+
+        this.selectSound = Galaga.getContext().getResource().get(GalagaSound.menu_select);
+        if (this.selectSound == null) {
+            return false;
+        }
+        this.selectSound.setCapacity(4);
+
+        int margin = 50;
 
         this.sky = new Sky(Config.SIZE_SKY_GRID);
         if (!this.sky.init()) {
@@ -59,6 +80,19 @@ public class EditorMenu extends Page<GalagaPage> {
                 this.size.getWidth() / 2,
                 this.size.getHeight() / 2));
 
+        this.back = new Text(
+                "BACK",
+                Position.of(
+                        this.size.getWidth() / 2,
+                        this.editSelect.getPosition().getY() + this.editSelect.getSize().getHeight() + margin),
+                Color.WHITE, this.titleFont);
+        if (!this.back.init()) {
+            return false;
+        }
+        this.back.setCenter(TextPosition.CENTER, TextPosition.END);
+
+        this.option = MenuEditorOption.EDITSELECT;
+        this.updateMenuSelect();
 
         return true;
     }
@@ -75,8 +109,7 @@ public class EditorMenu extends Page<GalagaPage> {
         if (Galaga.getContext().getInput().isKeyPressed(KeyEvent.VK_LEFT)) {
             this.selectSound.play(2.f);
             switch (this.option) {
-                case GAMEMODE -> this.gameMode.prev();
-                case SHIPSKIN -> this.shipSelect.prev();
+                case EDITSELECT -> this.editSelect.prev();
                 default -> {
                 }
             }
@@ -84,58 +117,42 @@ public class EditorMenu extends Page<GalagaPage> {
         } else if (Galaga.getContext().getInput().isKeyPressed(KeyEvent.VK_RIGHT)) {
             this.selectSound.play(2.f);
             switch (this.option) {
-                case GAMEMODE -> this.gameMode.next();
-                case SHIPSKIN -> this.shipSelect.next();
+                case EDITSELECT -> this.editSelect.next();
                 default -> {
                 }
             }
         } else if (Galaga.getContext().getInput().isKeyPressed(KeyEvent.VK_UP)) {
             this.selectSound.play(2.f);
             switch (this.option) {
-                case GAMEMODE -> {
-                    this.option = MenuOption.QUIT;
+                case EDITSELECT -> {
+                    this.option = MenuEditorOption.BACK;
                     this.updateMenuSelect();
                 }
-                case SHIPSKIN -> {
-                    this.option = MenuOption.GAMEMODE;
-                    this.updateMenuSelect();
-                }
-                case QUIT -> {
-                    this.option = MenuOption.SHIPSKIN;
+                case BACK -> {
+                    this.option = MenuEditorOption.EDITSELECT;
                     this.updateMenuSelect();
                 }
             }
         } else if (Galaga.getContext().getInput().isKeyPressed(KeyEvent.VK_DOWN)) {
             this.selectSound.play(2.f);
             switch (this.option) {
-                case GAMEMODE -> {
-                    this.option = MenuOption.SHIPSKIN;
+                case EDITSELECT -> {
+                    this.option = MenuEditorOption.BACK;
                     this.updateMenuSelect();
 
                 }
-                case SHIPSKIN -> {
-                    this.option = MenuOption.QUIT;
-                    this.updateMenuSelect();
-
-                }
-                case QUIT -> {
-                    this.option = MenuOption.GAMEMODE;
+                
+                case BACK -> {
+                    this.option = MenuEditorOption.EDITSELECT;
                     this.updateMenuSelect();
                 }
             }
         } else if (Galaga.getContext().getInput().isKeyPressed(KeyEvent.VK_ENTER)) {
             this.selectSound.play(2.f);
             switch (this.option) {
-                case QUIT -> Galaga.getContext().getApplication().stop();
-                case GAMEMODE -> {
-
-                    if (this.gameMode.getSelected().getText().equals(MenuModeOption.SOLO.toString())) {
-                        Galaga.getContext().getState().shipSkin = this.shipSelect.getSelected().getSprite();
-                        Galaga.getContext().getApplication().setCurrentPage(GalagaPage.GAME_SOLO);
-
-                    } else if (this.gameMode.getSelected().getText().equals(MenuModeOption.EDITOR.toString())) {
-                        Galaga.getContext().getApplication().setCurrentPage(GalagaPage.EDITOR_MENU);
-                    }
+                case BACK -> Galaga.getContext().getApplication().setCurrentPage(GalagaPage.MAIN_MENU);
+                case EDITSELECT -> {
+                    
                 }
                 default -> {
                 }
@@ -143,9 +160,31 @@ public class EditorMenu extends Page<GalagaPage> {
         }
     }
 
+
+
+
+    private void updateMenuSelect() {
+        switch (this.option) {
+            case EDITSELECT -> {
+                this.editSelect.setShowArrows(true);
+
+                this.editSelect.setColor(Color.ORANGE);
+                this.back.setColor(Color.WHITE);
+            }
+            case BACK -> {
+                this.editSelect.setShowArrows(false);
+
+                this.editSelect.setColor(Color.WHITE);
+                this.back.setColor(Color.ORANGE);
+            }
+        }
+    }
+
     @Override
     public void draw() {
+        this.sky.draw();
         this.editSelect.draw();
+        this.back.draw();
         // < level, sprite, settings>
         // back
     }
